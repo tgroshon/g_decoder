0afe6894a602022d0525295de72fb5eb96e3cfcd
Tommy Groshong <tommy.groshong@monetago.com>
2022-12-03 19:27:41 -0500

    Initial Commit

---
+++ lib/g_decoder.ex
@@ -0,0 +1,18 @@
+defmodule GDecoder do
+  @moduledoc """
+  Documentation for `GDecoder`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GDecoder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/g_decoder_test.exs
@@ -0,0 +1,8 @@
+defmodule GDecoderTest do
+  use ExUnit.Case
+  doctest GDecoder
+
+  test "greets the world" do
+    assert GDecoder.hello() == :world
+  end
+end
